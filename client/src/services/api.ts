4fdca035db2b7eef0225ac4ccbf4d815693ec05b
kp <kanchanprajapat676@gmail.com>
2026-07-20 21:54:56 +0530

    client api url fixed

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,81 +1,47 @@
 import axios from "axios";
-
 import { useAuthStore } from "@/auth/stores/authStore";
-import "./env";
-const api = axios.create({
 
-  baseURL:
-    const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
+
+const api = axios.create({
+  baseURL: API_URL,
 
   headers: {
-
-    "Content-Type":
-      "application/json",
-
+    "Content-Type": "application/json",
   },
-
 });
-
-
 
 /* -------------------------------
    Request Interceptor
 -------------------------------- */
 
 api.interceptors.request.use(
-
   (config) => {
-
-    const token =
-      useAuthStore
-        .getState()
-        .token;
+    const token = useAuthStore.getState().token;
 
     if (token) {
-
-      config.headers.Authorization =
-        `Bearer ${token}`;
-
+      config.headers.Authorization = `Bearer ${token}`;
     }
 
     return config;
-
   },
-
-  (error) =>
-
-    Promise.reject(error)
-
+  (error) => Promise.reject(error)
 );
-
-
 
 /* -------------------------------
    Response Interceptor
 -------------------------------- */
 
 api.interceptors.response.use(
-
   (response) => response,
 
   (error) => {
-
-    if (
-
-      error.response?.status === 401
-
-    ) {
-
-      useAuthStore
-        .getState()
-        .logout();
-
+    if (error.response?.status === 401) {
+      useAuthStore.getState().logout();
     }
 
     return Promise.reject(error);
-
   }
-
 );
 
 export default api;
